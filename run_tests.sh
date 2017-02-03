cd44ac59e09c0532fd345b8a5373a6ff42ad704c
Dat Tran <datitran@gmail.com>
2017-02-03 15:16:27 +0100

    WIP Rechange order of run script.

--- run_tests.sh
+++ run_tests.sh
@@ -7,9 +7,13 @@ set -e
 
 echo "Hello World"
 
+apt-get update
+apt-get -y install wget curl software-properties-common bzip2
+
+# install openjdk
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
-apt-get -y install wget curl software-properties-common bzip2 openjdk-8-jdk
+apt-get -y install openjdk-8-jdk
 
 # Install Miniconda
 wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh \
