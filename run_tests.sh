e9d279e0842f0052c986a281cf5adcbfcf49cc6b
Dat Tran <datitran@gmail.com>
2017-02-06 10:09:59 +0100

    WIP: Change the export for SPARK_HOME.
    Signed-off-by: Andreas Fleig <andreasf@pivotal.io>

--- run_tests.sh
+++ run_tests.sh
@@ -3,19 +3,16 @@
 SPARK_URL=http://d3kbcqa49mib13.cloudfront.net/spark-2.1.0-bin-hadoop2.7.tgz
 SPARK_FILENAME=spark-2.1.0-bin-hadoop2.7.tgz
 
-set -e
+set -ex
 
-echo "Hello World"
+apt-get update && \
+    apt-get -y install wget curl software-properties-common bzip2 && \
+    # install openjdk
+    add-apt-repository -y ppa:openjdk-r/ppa && \
+    apt-get update && \
+    apt-get -y install openjdk-8-jdk
 
-apt-get update
-apt-get -y install wget curl software-properties-common bzip2
-
-# install openjdk
-add-apt-repository -y ppa:openjdk-r/ppa
-apt-get update
-apt-get -y install openjdk-8-jdk
-
-echo "Install miniconda"
+echo "Install Miniconda"
     wget --quiet https://repo.continuum.io/miniconda/Miniconda3-4.1.11-Linux-x86_64.sh -O ~/miniconda.sh
     bash ~/miniconda.sh -b -p /home/conda
     rm ~/miniconda.sh
@@ -23,10 +20,10 @@ echo "Install miniconda"
     conda install -y nose numpy pandas
 
 # Install Spark
-curl $SPARK_URL > $HOME/$SPARK_FILENAME
-tar -xzf $HOME/$SPARK_FILENAME -C $HOME/
-
-echo 'export SPARK_HOME=$HOME/spark-2.1.0-bin-hadoop2.7/' >> $HOME/.bashrc && source $HOME/.bashrc
+echo "Install Spark"
+    curl $SPARK_URL > $HOME/$SPARK_FILENAME
+    tar -xzf $HOME/$SPARK_FILENAME -C $HOME/
+    export SPARK_HOME="$HOME/spark-2.1.0-bin-hadoop2.7/"
 
 nosetests -vs pyspark-app-ci/tests/test_linear_regression.py
 
